87e8a00d2036a7f6344228e781483ba3d04a3003
Islam Nurmukhametov <islam.nurmuhametov@omega-r.com>
2023-09-03 01:50:22 +0300

    completed challenge

---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Ислам Нурмухаметов on 02.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
